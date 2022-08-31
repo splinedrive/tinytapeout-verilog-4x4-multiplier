95fff05811580d9a6b0bcb48564205694694b3fe
Hirosh Dabui <hirosh@dabui.de>
2022-08-31 11:37:42 +0200

    integrated a 4x4 bit unsigned multiplier

--- src/user_module_341154068332282450.v
+++ src/user_module_341154068332282450.v
@@ -1,56 +1,96 @@
 `default_nettype none
+module user_module_341493393195532884(
+        input [7:0] io_in,
+        output [7:0] io_out
+    );
+    wire [3:0] a = io_in[7:4];
+    wire [3:0] b = io_in[3:0];
+    wire [7:0] c;
+    assign io_out = c;
 
-//  Top level io for this module should stay the same to fit into the scan_wrapper.
-//  The pin connections within the user_module are up to you,
-//  although (if one is present) it is recommended to place a clock on io_in[0].
-//  This allows use of the internal clock divider if you wish.
-module user_module_341154068332282450(
-  input [7:0] io_in, 
-  output [7:0] io_out
-);
-
-  wire pdm_out;
-
-  assign io_out[0] = pdm_out;
-  assign io_out[1] = ~pdm_out;
-
-  pdm_341154068332282450 pdm_core(
-    .pdm_input(io_in[7:3]),
-    .write_en(io_in[2]),
-    .reset(io_in[1]),
-    .clk(io_in[0]),    
-    .pdm_out(pdm_out)
-  );
+    mul #(4) mul_i(a, b, c);
 
 endmodule
 
-//  Any submodules should be included in this file,
-//  so they are copied into the main TinyTapeout repo.
-//  Appending your ID to any submodules you create 
-//  ensures there are no clashes in full-chip simulation.
-module pdm_341154068332282450(
-    input [4:0] pdm_input,
-    input       write_en,
-    input       clk, reset,    
-    output      pdm_out
-);
+/*
+ *  my_multiplier - an unoptimized multiplier
+ *
+ *  copyright (c) 2021  hirosh dabui <hirosh@dabui.de>
+ *
+ *  permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  the software is provided "as is" and the author disclaims all warranties
+ *  with regard to this software including all implied warranties of
+ *  merchantability and fitness. in no event shall the author be liable for
+ *  any special, direct, indirect, or consequential damages or any damages
+ *  whatsoever resulting from loss of use, data or profits, whether in an
+ *  action of contract, negligence or other tortious action, arising out of
+ *  or in connection with the use or performance of this software.
+ *
+ */
+module mul
+    #(
+         parameter WIDTH = 16
+     )
+     (
+         input      [WIDTH-1:0]      a,
+         input      [WIDTH-1:0]      b,
+         output reg [(WIDTH<<1)-1:0] c = 0
+     );
 
-reg [4:0] accumulator;
-reg [4:0] input_reg;
+    reg [(WIDTH<<1)-1:0] tmp;
 
-wire [5:0] sum;
+    reg  [(WIDTH<<1)-1:0] add_a[WIDTH-1:0];
+    reg  [(WIDTH<<1)-1:0] add_b[WIDTH-1:0];
+    wire [(WIDTH<<1)-1:0] add_y[WIDTH-1:0];
 
-assign sum = input_reg + accumulator;
-assign pdm_out = sum[5];
+    genvar k;
+generate for (k = 0; k < WIDTH; k = k +1) begin
+            full_addr #(WIDTH<<1) full_addr_i(add_a[k], add_b[k], add_y[k]);
+end endgenerate
 
-always @(posedge clk or posedge reset) begin
-    if (reset) begin 
-        input_reg <= 5'h00 ;
-        accumulator <= 5'h00;
-    end else begin
-        accumulator <= sum[4:0];
-        if (write_en) input_reg <= pdm_input ;
-    end
-end
+    integer i;
+    integer j;
+generate always @(*) begin
+            tmp = 0;
+            c   = 0;
+
+            /* generate parallel structure */
+            for (j = 0; j < WIDTH; j = j + 1) begin
+                for (i = 0; i < WIDTH; i = i + 1) begin
+                    tmp[i] = b[i] & a[j];
+                end
+                add_a[j] = c;
+                add_b[j] = (tmp << j);
+                c = add_y[j];
+            end
+        end endgenerate
+
+endmodule
+
+// carry ripple style
+module full_addr
+    #(
+         parameter WIDTH = 16
+     )
+     (
+         input      [WIDTH-1:0] a,
+         input      [WIDTH-1:0] b,
+         output reg [WIDTH-1:0] y = 0
+     );
+
+    integer i;
+    reg [WIDTH-1:0] c = 1;
+generate always @(*) begin
+            c[0] = 0;
+            y[0] = c[0] ^ (a[0] ^ b[0]);
+            c[0] = a[0]&b[0] | b[0]&c[0] | a[0]&c[0];
+            for (i = 1; i < WIDTH; i = i +1) begin
+                y[i] = c[i -1] ^ (a[i] ^ b[i]);
+                c[i] = a[i]&b[i] | b[i]&c[i -1] | a[i]&c[i -1];
+            end
+        end endgenerate
 
 endmodule
